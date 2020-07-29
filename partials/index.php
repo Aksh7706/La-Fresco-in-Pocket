dda445b6172a08f86e3b195d0904b723082df361
KRISHANU SAINI <56930593+krishanu-2001@users.noreply.github.com>
2020-07-29 14:11:10 +0530

    added index.php and catalog.php

---
+++ partials/index.php
@@ -0,0 +1,15 @@
+// contains all cdn required
+//  bootstrap
+//  font-awesome
+<html>
+<head>
+    <title>
+        it1
+    </title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="public\font-awesome-4.7.0\css\font-awesome.min.css">
+</head>
+</html>
